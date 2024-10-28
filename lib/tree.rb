ee4b3bf133d618e1969b8ee92631a540dd78834a
Jmara <jmara.0923@gmail.com>
2024-10-28 21:43:31 +0200

    Add instance variable for sorted array

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,16 +1,17 @@
+require_relative 'node'
+
 class Tree
   def initialize(array)
-    @array = array
+    @sorted_array = array.sort.uniq
     @start = 0
-    @end = @array.length - 1
-    @root = build_tree(@array, @start, @end)
+    @end = @sorted_array.length - 1
+    @root = build_tree(@sorted_array, @start, @end)
   end
 
   def build_tree(array, start, last)
-    sorted_array = @array.sort.uniq
     return nil if start > last
 
-    @mid = start + last / 2
+    @mid = (start + last) / 2
     @root = Node.new(array[@mid])
     @root.left = build_tree(array, start, mid - 1)
     @root.right = build_tree(array, mid + 1, last)
@@ -18,9 +19,8 @@ class Tree
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
-   pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-   puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
- end
-
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
